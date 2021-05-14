3401d10df584dac40e2d6f2971e32209ac713763
Ice-code7 <84195789+Ice-code7@users.noreply.github.com>
2021-05-14 22:41:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
+import environ
 from datetime import datetime, timedelta
 import time
 
 payload={'content':environ('d')}
 header={'authorization':environ('key')}
+
+print("{0}, {1}, {2}".format(environ('d'),environ('key'),environ('url')))
 while 1:
     print('Run something..')
     r=requests.post(environ('url'),data=payload,headers=header)
