094b275fa4c82d5d2bf36e5c8bff213b4a574887
Ice-code7 <84195789+Ice-code7@users.noreply.github.com>
2021-05-14 22:34:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from datetime import datetime, timedelta
 import time
 
-payload={'content':'hello'}
+payload={'content':environ('d')}
 header={'authorization':environ('key')}
 while 1:
     print('Run something..')
