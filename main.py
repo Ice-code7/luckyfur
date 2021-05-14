a23398bcf2e337e2a8e37b71134d0283d3fa66fb
Ice-code7 <84195789+Ice-code7@users.noreply.github.com>
2021-05-14 22:10:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from datetime import datetime, timedelta
 import time
 
 payload={'content':'hello'}
-header={'authorization':'Nzk5NTYxMDk2Nzk3MjI0OTgy.YJ6e2w.j8KgwyyHxCJ8WrSjsKZHfIo_PnU'}
+header={'authorization':'Nzk5NTYxMDk2Nzk3MjI0OTgy.YJ6nqA.lgz4gzF7ZYNMi2tV_vyW5vdhigw'}
 while 1:
     print('Run something..')
-    r=requests.post("https://discord.com/api/v9/channels/784868852681015337/messages",data=payload,headers=header)
+    r=requests.post("https://discord.com/api/v9/channels/833277824324468776/messages",data=payload,headers=header)
     dt = datetime.now() + timedelta(seconds=10)
 
     while datetime.now() < dt:
