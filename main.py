0750a64a938d2c8555accfd7636e3931ccf0467a
Ice-code7 <84195789+Ice-code7@users.noreply.github.com>
2021-05-14 22:59:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ print("{0}, {1}, {2}".format(d,key,url))
 while 1:
     print('Run something..')
     r=requests.post(url,data=payload,headers=header)
+    print(r)
     dt = datetime.now() + timedelta(hours=1)
 
     while datetime.now() < dt:
